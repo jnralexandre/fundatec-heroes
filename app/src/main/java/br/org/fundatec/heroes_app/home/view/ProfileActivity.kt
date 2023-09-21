4e81e7a4625fd99dafc365251438f8ebd4051ffb
jnralexandre <alexandreoliveirajnr@gmail.com>
2023-09-21 00:58:56 -0300

    Finaliza primeira etapa da activity_profile.xml

--- app/src/main/java/br/org/fundatec/heroes_app/home/view/ProfileActivity.kt
+++ app/src/main/java/br/org/fundatec/heroes_app/home/view/ProfileActivity.kt
@@ -1,8 +1,15 @@
 package br.org.fundatec.heroes_app.home.view
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import br.org.fundatec.heroes_app.R
+import com.google.android.material.snackbar.Snackbar
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -10,6 +17,42 @@ class ProfileActivity : AppCompatActivity() {
         getSupportActionBar()?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        findViewById<Button>(R.id.btn_criar_usuario).setOnClickListener() {
+            mostrarSnackbar3()
+        }
     }
+
+    private fun mostrarSnackbar() {
+        Snackbar.make(findViewById(android.R.id.content), R.string.app_name, Snackbar.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun mostrarSnackbar2(view: View) {
+        Snackbar.make(view, R.string.app_name, Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun mostrarSnackbar3() {
+        Snackbar.make(
+            findViewById(android.R.id.content),
+            R.string.usuario_criado,
+            Snackbar.LENGTH_INDEFINITE
+        )
+            .setAction(android.R.string.ok) {
+                val intent = Intent(this@ProfileActivity, HomeActivity::class.java)
+                startActivity(intent)
+            }
+            .setActionTextColor(
+                ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.white
+                    )
+                )
+            )
+            .setBackgroundTint(ContextCompat.getColor(this, R.color.laranja_fundatec))
+            .show()
+    }
+
 
 }
