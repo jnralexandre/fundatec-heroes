d1c6f747bbab48276479ccded611cea11b4d1f33
jnralexandre <alexandreoliveirajnr@gmail.com>
2023-09-26 23:09:58 -0300

    Finaliza ProfileActivity

--- app/src/main/java/br/org/fundatec/heroes_app/home/view/ProfileActivity.kt
+++ app/src/main/java/br/org/fundatec/heroes_app/home/view/ProfileActivity.kt
@@ -2,57 +2,66 @@ package br.org.fundatec.heroes_app.home.view
 
 import android.content.Intent
 import android.content.res.ColorStateList
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import br.org.fundatec.heroes_app.R
 import com.google.android.material.snackbar.Snackbar
 
 class ProfileActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         getSupportActionBar()?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
         findViewById<Button>(R.id.btn_criar_usuario).setOnClickListener() {
-            mostrarSnackbar3()
+            if (findViewById<EditText>(R.id.edit_text_nome).getText().length == 0) {
+                mostrarSnackbar("edit_text_nome")
+            } else if (findViewById<EditText>(R.id.edit_text_email).getText().length == 0) {
+                mostrarSnackbar("edit_text_email")
+            } else if (findViewById<EditText>(R.id.edit_text_senha).getText().length < 8) {
+                mostrarSnackbar("edit_text_senha")
+            } else {
+                mostrarSnackbar("btn_criar_usuario");
+            }
         }
     }
 
-    private fun mostrarSnackbar() {
-        Snackbar.make(findViewById(android.R.id.content), R.string.app_name, Snackbar.LENGTH_SHORT)
-            .show()
-    }
-
-    private fun mostrarSnackbar2(view: View) {
-        Snackbar.make(view, R.string.app_name, Snackbar.LENGTH_LONG).show()
-    }
-
-    private fun mostrarSnackbar3() {
-        Snackbar.make(
-            findViewById(android.R.id.content),
-            R.string.usuario_criado,
-            Snackbar.LENGTH_INDEFINITE
-        )
-            .setAction(android.R.string.ok) {
+    private fun mostrarSnackbar(name: String) {
+        if (name.equals("edit_text_nome")) {
+            Snackbar.make(
+                findViewById(android.R.id.content),
+                R.string.informe_nome_de_usuario,
+                Snackbar.LENGTH_LONG,
+            ).setBackgroundTint(ContextCompat.getColor(this, R.color.vermelho)).show()
+            findViewById<EditText>(R.id.edit_text_nome).setError("Informe um nome de usuário");
+        } else if (name.equals("edit_text_email")) {
+            Snackbar.make(
+                findViewById(android.R.id.content), R.string.informe_e_mail, Snackbar.LENGTH_LONG
+            ).setBackgroundTint(ContextCompat.getColor(this, R.color.vermelho)).show()
+            findViewById<EditText>(R.id.edit_text_email).setError("Informe um e-mail");
+        } else if (name.equals("edit_text_senha")) {
+            Snackbar.make(
+                findViewById(android.R.id.content), R.string.informe_senha, Snackbar.LENGTH_LONG
+            ).setBackgroundTint(ContextCompat.getColor(this, R.color.vermelho)).show()
+            findViewById<EditText>(R.id.edit_text_senha).setError("A senha precisa conter no mínimo 8 caracteres");
+        } else {
+            Snackbar.make(
+                findViewById(android.R.id.content),
+                R.string.usuario_criado,
+                Snackbar.LENGTH_INDEFINITE
+            ).setAction(android.R.string.ok) {
                 val intent = Intent(this@ProfileActivity, HomeActivity::class.java)
                 startActivity(intent)
-            }
-            .setActionTextColor(
+            }.setActionTextColor(
                 ColorStateList.valueOf(
                     ContextCompat.getColor(
-                        this,
-                        R.color.white
+                        this, R.color.white
                     )
                 )
-            )
-            .setBackgroundTint(ContextCompat.getColor(this, R.color.laranja_fundatec))
-            .show()
+            ).setBackgroundTint(ContextCompat.getColor(this, R.color.laranja_fundatec)).show()
+        }
     }
-
-
 }
